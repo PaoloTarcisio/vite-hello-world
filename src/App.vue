791f095df1f601e3f34e2aa9026be417c8e44233
PaoloTarcisio <paoloc50@gmail.com>
2024-01-17 15:06:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import viteHelloWorld from './components/viteHelloWorld.vue';
+
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        components: {
+            viteHelloWorld
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1><viteHelloWorld/></h1>
+    </div>
+</template>
+
+<style scoped>
+</style>
